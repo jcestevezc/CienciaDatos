fbfe6c652b98f496693320caeebed2a90967c865
CO1012351486 <CO1012351486@COLCOD7L069791.enelint.global>
2018-10-07 14:28:27 -0500

    Descarga y almacenamiento en base de datos

--- Test/TestEnv.py
+++ Test/TestEnv.py
@@ -10,18 +10,22 @@ import requests
 import pandas as pd
 import psycopg2
 
-sql_digital_press = "INSERT INTO DS_DIGITAL_PRESS(TITLE,AUTHOR,DATE,MEDIO,TYPE_ARTICLE,KEY_WORDS) VALUES(%s)"
-connection  = psycopg2.connect(host="localhost",database="data_science", user="postgres", password="root")
-cursor = connection.cursor()
-#print('PostgreSQL database version:')
-cursor.execute('SELECT version()')
-print(type(cursor))
+connection  = psycopg2.connect(
+    host="localhost",
+    database="data_science", 
+    user="postgres", 
+    password="root"
+)
 
+cursor = connection.cursor()
+sql_digital_press = 'INSERT INTO "DS_DIGITAL_PRESS" ("TITLE", "AUTHOR", "DATE", "MEDIO", "TYPE_ARTICLE", "KEY_WORDS","CONTENT")  VALUES ('
 r = requests.get("https://www.larepublica.co/economia/ministerio-de-comercio-presento-estrategia-estado-simple-colombia-agil-2777011")
 root = lxml.html.fromstring(r.content)
 
+r = requests.get("http://www.elcolombiano.com/colombia/los-danos-de-la-mineria-ilegal-en-el-medio-ambiente-antioqueno-GY9450967")
+root2 = lxml.html.fromstring(r.content)
 
-def laRepublica(root):
+def laRepublica(root,sql_digital_press):
     title = root.xpath("//meta[@property='og:title']/@content")
     content = root.xpath("//div[contains(@class,'articleWrapper')]")
     author = root.xpath("//div[@class='autorArticle']")
@@ -38,17 +42,59 @@ def laRepublica(root):
                 'type_article':type_article,
                 'key_words':key_words}
     
+    register = clean(dictionary)
+    sql_digital_press = sql_digital_press +"'"+ register.title.values[0] + "','"+str(register.author.values[0]) + "','"+ str(register.date.values[0]) + "','"+ str(register.medio.values[0]) + "','"+ str(register.type_article.values[0]) + "','"+ str(register.key_words.values[0]) + "','"+ str(register.content.values[0]) + "');"
+    cursor.execute(sql_digital_press)
+    
+def elColombiano(root2,sql_digital_press):
+    title = root.xpath("//meta[@property='og:title']")
+    content = root.xpath("//article[@class='articulo fontChange no-lateral']")
+    author = root.xpath("//meta[@name='author']")
+    date = root.xpath("//div[@class='autor']//h6")
+    medio = root.xpath("//meta[@property='og:site_name']")
+    type_article = root.xpath("//meta[@property='og:type']/@content") 
+    key_words = root.xpath("//meta[@name='keywords']/@content")
+    
+    dictionary = {'title': title, 
+                'content': content, 
+                'author':author,
+                'date':date,
+                'medio':medio,
+                'type_article':type_article,
+                'key_words':key_words}
+    
+    print(dictionary)
+    input()
+    register = clean(dictionary)
+    sql_digital_press = sql_digital_press +"'"+ register.title.values[0] + "','"+str(register.author.values[0]) + "','"+ str(register.date.values[0]) + "','"+ str(register.medio.values[0]) + "','"+ str(register.type_article.values[0]) + "','"+ str(register.key_words.values[0]) + "','"+ str(register.content.values[0]) + "');"
+    cursor.execute(sql_digital_press)
+    
+def clean(dictionary):
     register = pd.DataFrame(data = dictionary)
-    #register.to_sql('DS_DIGITAL_PRESS', con = connection, if_exists='append')
-    cursor.executemany(sql_digital_press, register)
+    register.title = str(register.title.values[0]).replace("[", "").replace("]", "")
+    register.content = str(register.content.values[0]).replace("[", "").replace("]", "")
+    register.author = str(register.author.values[0]).replace("[", "").replace("]", "")
+    register.date = str(register.date.values[0]).replace("[", "").replace("]", "")
+    register.medio = str(register.medio.values[0]).replace("[", "").replace("]", "")
+    register.type_article = str(register.type_article.values[0]).replace("[", "").replace("]", "")
+    register.key_words = str(register.key_words.values[0]).replace("[", "").replace("]", "")
+    return register
 
 
-laRepublica(root)
+#laRepublica(root,sql_digital_press)
+elColombiano(root2,sql_digital_press)
+
+cursor.execute('SELECT * FROM public."DS_DIGITAL_PRESS"')
+rows = cursor.fetchall()
+for row in rows:
+    print("Registro"+format(row))
+
+
 
 connection.commit()
 cursor.close()
 connection.close()
 
-#file = pd.read_csv("pages.csv", sep=";")
-#print(file.iloc[0])
 
+#connection.autocommit = True
+#connection.closed
