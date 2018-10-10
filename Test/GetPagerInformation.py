481d211a205ad69bbad56fe88e3c26fb01e82d3f
CO1012351486 <CO1012351486@COLCOD7L069791.enelint.global>
2018-10-10 05:43:14 -0500

    Versión estable, continua sin traer el texto del articulo

--- Test/GetPagerInformation.py
+++ Test/GetPagerInformation.py
@@ -42,18 +42,23 @@ def laRepublica(root,sql_digital_press):
                 'type_article':type_article,
                 'key_words':key_words}
     
+    print(dictionary)
+    input()
     register = clean(dictionary)
     sql_digital_press = sql_digital_press +"'"+ register.title.values[0] + "','"+str(register.author.values[0]) + "','"+ str(register.date.values[0]) + "','"+ str(register.medio.values[0]) + "','"+ str(register.type_article.values[0]) + "','"+ str(register.key_words.values[0]) + "','"+ str(register.content.values[0]) + "');"
     cursor.execute(sql_digital_press)
     
-def elColombiano(root2,sql_digital_press):
-    title = root.xpath("//meta[@property='og:title']")
-    content = root.xpath("//article[@class='articulo fontChange no-lateral']")
-    author = root.xpath("//meta[@name='author']")
+def elColombiano(root,sql_digital_press):
+    title = root.xpath("//meta[@property='og:title']/@content")
+    content = root.xpath("//div[@class='text']")
+    content = format(content[0].text.strip())
+    author = root.xpath("//meta[@name='author']/@content")
     date = root.xpath("//div[@class='autor']//h6")
-    medio = root.xpath("//meta[@property='og:site_name']")
+    date = format(date[0].text.strip())
+    medio = root.xpath("//meta[@property='og:site_name']/@content")
     type_article = root.xpath("//meta[@property='og:type']/@content") 
     key_words = root.xpath("//meta[@name='keywords']/@content")
+    key_words = str(key_words)
     
     dictionary = {'title': title, 
                 'content': content, 
@@ -61,23 +66,25 @@ def elColombiano(root2,sql_digital_press):
                 'date':date,
                 'medio':medio,
                 'type_article':type_article,
-                'key_words':key_words}
+                'key_words':key_words
+    }
     
-    print(dictionary)
-    input()
+    #print(dictionary)
+    #input()
     register = clean(dictionary)
     sql_digital_press = sql_digital_press +"'"+ register.title.values[0] + "','"+str(register.author.values[0]) + "','"+ str(register.date.values[0]) + "','"+ str(register.medio.values[0]) + "','"+ str(register.type_article.values[0]) + "','"+ str(register.key_words.values[0]) + "','"+ str(register.content.values[0]) + "');"
+    print(sql_digital_press)
     cursor.execute(sql_digital_press)
     
 def clean(dictionary):
     register = pd.DataFrame(data = dictionary)
-    register.title = str(register.title.values[0]).replace("[", "").replace("]", "")
-    register.content = str(register.content.values[0]).replace("[", "").replace("]", "")
-    register.author = str(register.author.values[0]).replace("[", "").replace("]", "")
-    register.date = str(register.date.values[0]).replace("[", "").replace("]", "")
-    register.medio = str(register.medio.values[0]).replace("[", "").replace("]", "")
-    register.type_article = str(register.type_article.values[0]).replace("[", "").replace("]", "")
-    register.key_words = str(register.key_words.values[0]).replace("[", "").replace("]", "")
+    register.title = str(register.title.values[0]).replace("[", "").replace("]", "").replace("  ", "")
+    register.content = str(register.content.values[0]).replace("[", "").replace("]", "").replace("  ", "")
+    register.author = str(register.author.values[0]).replace("[", "").replace("]", "").replace("  ", "")
+    register.date = str(register.date.values[0]).replace("[", "").replace("]", "").replace("  ", "").replace("\t", "").replace("\n", "").replace("\r", "")
+    register.medio = str(register.medio.values[0]).replace("[", "").replace("]", "").replace("  ", "")
+    register.type_article = str(register.type_article.values[0]).replace("[", "").replace("]", "").replace("  ", "")
+    register.key_words = str(register.key_words.values[0]).replace("[", "").replace("]", "").replace("  ", "").replace("'", "")
     return register
 
 
